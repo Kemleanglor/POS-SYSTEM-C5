d9375649ab7b35081c5bba42ed7c174993b89ace
sreyvoath <141004103+sreyvoath@users.noreply.github.com>
2023-12-15 10:38:01 +0700

    #9 add-category

---
+++ JS/category.js
@@ -0,0 +1,15 @@
+ let btnAddcategory = document.getElementById("btn-add");
+ let searchCategory = document.getElementById("search");
+ let tbody = document.querySelectorAll("tbody");
+
+
+
+
+function createcategory(){
+    let tr = document.createElement("tr");
+    let tdProductID = document.createElement("td");
+    let tdcategoryName = document.createElement("td");
+    let tdAction = document.createElement("td");
+    
+    
+}
