a1db8ff74878bdaffad75e2aa6faf29a796482e3
Kemleanglor <kemleang.lor@student.passerellesnumeriques.org>
2023-12-15 12:41:00 +0700

    #10update icon

--- JS/product.js
+++ JS/product.js
@@ -1,123 +1,140 @@
-// // -----------------Show and Hid funtions ---------------------
-// function show(element) {
-//     element.style.display = "block";
-// }
-// function hid(element) {
-//     element.style.display = "none";
-// }
+// -----------------Show and Hid funtions ---------------------
+function show(element) {
+    element.style.display = "block";
+}
+function hid(element) {
+    element.style.display = "none";
+}
 
-// //---------------------Save and load Product ---------------------
-// function saveProducts() {
-//     localStorage.setItem("products", JSON.stringify(productData.products));
-// }
+//---------------------Save and load Product ---------------------
+function saveProducts() {
+    localStorage.setItem("products", JSON.stringify(productData.products));
+}
 
-// function loadProducts() {
-//     let productsStorage = JSON.parse(localStorage.getItem("products"));
-//     if (productsStorage !== null) {
-//         products = productsStorage;
-//     }
-// }
+function loadProducts() {
+    let productsStorage = JSON.parse(localStorage.getItem("products"));
+    if (productsStorage !== null) {
+        products = productsStorage;
+    }
+}
 
-// //---------------------Cancel and Save funtion ------------------
-// function cancel() {
-//     hid(dom_product_dialog);
-// }
+//---------------------Cancel and Save funtion ------------------
+function cancel() {
+    hid(dom_product_dialog);
+}
 
-// function addProduct() {
-//     hid(dom_product_dialog);
+function addProduct() {
+    hid(dom_product_dialog);
 
-//     let productId = productData.lastestId;
-//     if (productId === null || productData.products.length === 0) {
-//         productId = 1;
+    let productId = productData.lastestId;
+    if (productId === null || productData.products.length === 0) {
+        productId = 1;
 
-//     }
-//     else {
-//         productId = productId + 1
-//     }
-//     //---------------- update lastest ID to the product list----------
-//     productData.lastestId = productId;
+    }
+    else {
+        productId = productId + 1
+    }
+    //---------------- update lastest ID to the product list----------
+    productData.lastestId = productId;
 
-//     //---------------- create new product and add product to product list-------
-//     let prodcut = {
-//         id: productId,
-//         name: productName.value,
-//         categroy: cateGory.value,
-//         quanity: quanitys.value,
-//         price: unitPrice.value,
-//     }
+    //---------------- create new product and add product to product list-------
+    let prodcut = {
+        id: productId,
+        name: productName.value,
+        categroy: cateGory.value,
+        quanity: quanitys.value,
+        price: unitPrice.value,
+    }
 
-//     productData.products.push(prodcut);
+    productData.products.push(prodcut);
 
-//     saveProducts();
+    saveProducts();
 
-//     getProducts();
-// }
-// function getProducts() {
-//     loadProducts()
+    getProducts();
+}
+function getProducts() {
+    loadProducts()
 
-//     if (localStorage.length===0){
-//         return;
-//     }
+    if (localStorage.length===0){
+        return;
+    }
 
-//     let tbody = document.querySelector("tbody");
-//     tbody.remove();
+    let tbody = document.querySelector("tbody");
+    tbody.remove();
 
-//     let newTbody = document.createElement("tbody");
-//     for (let data of products){
+    let newTbody = document.createElement("tbody");
+    for (let data of products){
 
-//         let tRows = document.createElement("tr");
+        let tRows = document.createElement("tr");
 
-//         let tdId = document.createElement("td")
-//         let tdName = document.createElement("td");
-//         let tdCategory = document.createElement("td");
-//         let tdQuanity = document.createElement("td");
-//         let tdUnitprice = document.createElement("td");
-//         let tdTotbalprice = document.createElement("td");
-//         let tdAction = document.createElement("td");
+        let tdId = document.createElement("td")
+        let tdName = document.createElement("td");
+        let tdCategory = document.createElement("td");
+        let tdQuanity = document.createElement("td");
+        let tdUnitprice = document.createElement("td");
+        let tdTotbalprice = document.createElement("td");
+        let tdAction = document.createElement("td");
+
+        let icondelete = document.createElement("i");
+        let iconedit = document.createElement("i");
+        let iconvisit = document.createElement("i");
+        icondelete.classList.add("material-icons");
+        icondelete.textContent="delete";
+
+        iconedit.classList.add("material-icons");
+        iconedit.textContent = "edit";
+
+        iconvisit.classList.add("material-icons");
+        iconvisit.textContent = "visibility";
+
+        tdAction.appendChild(icondelete);
+        tdAction.appendChild(iconedit);
+        tdAction.appendChild(iconvisit);
+
         
 
-//         tdId.textContent = data.id;
-//         tdName.textContent = data.name;
-//         tdCategory.textContent = data.categroy;
-//         tdQuanity.textContent = data.quanity;
-//         tdUnitprice.textContent = data.price;
-//         tdTotbalprice.textContent = parseInt(data.quanity)*parseInt(data.price);
-//         tdAction.textContent = "Hello world";
+        tdId.textContent = data.id;
+        tdName.textContent = data.name;
+        tdCategory.textContent = data.categroy;
+        tdQuanity.textContent = data.quanity;
+        tdUnitprice.textContent = data.price;
+        tdTotbalprice.textContent = parseInt(data.quanity)*parseInt(data.price);
+        // tdAction.textContent = "Hello world";
 
-//         tRows.appendChild(tdId);
-//         tRows.appendChild(tdName);
-//         tRows.appendChild(tdCategory);
-//         tRows.appendChild(tdQuanity);
-//         tRows.appendChild(tdUnitprice);
-//         tRows.appendChild(tdTotbalprice);
-//         tRows.appendChild(tdAction);
+        tRows.appendChild(tdId);
+        tRows.appendChild(tdName);
+        tRows.appendChild(tdCategory);
+        tRows.appendChild(tdQuanity);
+        tRows.appendChild(tdUnitprice);
+        tRows.appendChild(tdTotbalprice);
+        tRows.appendChild(tdAction);
 
-//         newTbody.appendChild(tRows);
-//     }
-//     let table = document.querySelector("table");
-//     table.appendChild(newTbody)
-// }
+        newTbody.appendChild(tRows);
+    }
+    let table = document.querySelector("table");
+    table.appendChild(newTbody)
+}
 
 
 
-// //-----------------------Show Form to Add prodcut funtion-------------------
-// function btnAddproduct() {
-//     show(dom_product_dialog);
-// }
+//-----------------------Show Form to Add prodcut funtion-------------------
+function btnAddproduct() {
+    show(dom_product_dialog);
+}
 
-// let btn = document.querySelector("#add-product");
-// //------------------Get form input-----------------------------
-// let dom_product_dialog = document.querySelector("#product-dialog")
+let btn = document.querySelector("#add-product");
+//------------------Get form input-----------------------------
+let dom_product_dialog = document.querySelector("#product-dialog")
 
-// let productName = document.querySelector("#name");
-// let cateGory = document.querySelector("#category");
-// let quanitys = document.querySelector("#quanity");
-// let unitPrice = document.querySelector("#unit-price");
+let productName = document.querySelector("#name");
+let cateGory = document.querySelector("#category");
+let quanitys = document.querySelector("#quanity");
+let unitPrice = document.querySelector("#unit-price");
 
-// //---------------------Product Data -----------------------------
-// let productData = {
-//     products: [],
-//     lastestId: null,
-// }
-// btn.addEventListener("click", btnAddproduct)
-// getProducts()
+//---------------------Product Data -----------------------------
+let productData = {
+    products: [],
+    lastestId: null,
+}
+btn.addEventListener("click", btnAddproduct)
+getProducts()
