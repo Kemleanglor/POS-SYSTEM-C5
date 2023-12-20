70c46a991adc459a2eded8b672e09987a62148b0
BUNNARITHHHHH <bunnarith.phoeurn@student.passerellesnumeriques.org>
2023-12-20 12:31:56 +0700

    update-main-code

--- JS/category.js
+++ JS/category.js
@@ -1,39 +1,177 @@
+//========================= Get Product Data ================
+let productData = JSON.parse(localStorage.getItem("product"));
+console.log(productData)
+
+// ======================= HIDE / SHOW ====================
+function hid(element){
+    element.style.display ="none";
+    
+}
+function show(element){
+    element.style.display ="block";
+    
+}
+
+//===========================LOCAL STORAGE ==================
+
+function saveCategory() {
+    localStorage.setItem("category", JSON.stringify(CategoryData.category));
+    
+  }
+  
+  function loadCategory() {
+    let categoryStorage = JSON.parse(localStorage.getItem("category"));
+    if (categoryStorage !== null) {
+        CategoryData.category = categoryStorage;
+    }
+  }
+
+
+//========= Update the View======================
+
+function renderCategory(){
+    let tbody = document.querySelector("tbody");
+    tbody.remove();
+    let newTbody = document.createElement("tbody");
+    let datas =0;
+    for (let data of CategoryData.category){
+        let tRows= document.createElement("tr");
+        tRows.dataset.index=datas
+        let tdId = document.createElement("td");
+        let tdName=document.createElement("td");
+        let tdAction=document.createElement("td");
+        let iconDelete = document.createElement("i");
+        iconDelete.classList.add("material-icons");
+        iconDelete.textContent="delete";
+        iconDelete.addEventListener("click",removeCategory)
+
+        let iconEdit = document.createElement("i");
+        iconEdit.classList.add("material-icons");
+        iconEdit.textContent="edit";
+        iconEdit.addEventListener("click",editCategory)
+
+        let iconView = document.createElement("i");
+        iconView.classList.add("material-icons");
+        iconView.textContent="visibility";
+
+        tdId.textContent=data.id;
+        tdName.textContent=data.name;
+        tdAction.appendChild(iconDelete);
+        tdAction.appendChild(iconEdit);
+        tdAction.appendChild(iconView);
+
+        tRows.appendChild(tdId)
+        tRows.appendChild(tdName);
+        tRows.appendChild(tdAction);
+
+        newTbody.appendChild(tRows);
+
+        datas+=1
+    }
+
+    table.appendChild(newTbody);
+}
+
+
+//=======================Edit  and Remove Procduct==================
+function editCategory(event){
+    let index = event.target.parentElement.parentElement.dataset.index;
+    let categorys = CategoryData.category[index];
+    document.getElementById("name").value=categorys.name;
+    document.getElementById("dct").value=categorys.discription;
+
+    show(dialog_catagory);
+    let editHeader = document.querySelector("header");
+    editHeader.textContent="Edit Category";
+    let btnEdit = document.querySelector("footer").lastElementChild;
+    btnEdit.textContent="Edit";
+    
+    editIndex=index
+
+}
+
+function removeCategory(event){
+    let index = event.target.parentElement.parentElement.dataset.index;
+
+    CategoryData.category.splice(index,1);
+
+    saveCategory();
+
+    renderCategory();
+
+}
+
+
+// ==================Buntton CanCel and Add Category to array==========
+
+function OnCancel(){
+    hid(dialog_catagory)
+}
+
+function OnAdd(){
+    hid(dialog_catagory)
+
+    let categoryId = CategoryData.lastestId;
+    if (categoryId === null || CategoryData.category.length === 0) {
+        categoryId = 1;
+
+    }
+    else if (editIndex === null) {
+        categoryId= categoryId + 1
+    }
+    else  {
+        categoryId = categoryId
+    }
+    //---------------- update lastest ID to the product list----------
+    CategoryData.lastestId = categoryId;
+    let categories={};
+    categories.id=categoryId;
+    categories.name=document.getElementById("name").value;
+    categories.discription=document.getElementById("dct").value;
+    if (editIndex===null){
+        CategoryData.category.push(categories);
+    }
+    else{
+        CategoryData.category[editIndex]=categories;
+    }
+    editIndex=null;
+
+    let addHeader = document.querySelector("header");
+    addHeader.textContent="Create Category";
+    let btnAdd = document.querySelector("footer").lastElementChild;
+    btnAdd.textContent="Add";
+
+    //========= save category===========
+    saveCategory()
+
+    //========== Update the view=========
+    renderCategory()
+}
+
+
+function Addcategory(){
+    show(dialog_catagory)
+}
+
+
 let dialog_catagory = document.getElementById("category-dialog");
-let searchCategory = document.getElementById("search");
-let tbody = document.querySelectorAll("tbody");
+
 let btnAddcategory = document.getElementById("btn-add");
+btnAddcategory.addEventListener("click",Addcategory);
+let searchCategory = document.getElementById("search");
 
-btnAddcategory.addEventListener("click",createcategory);
+let table = document.querySelector("table")
+
+let editIndex=null;
+
+let CategoryData={
+    category:[],
+    lastestId:null,
+}
+
+loadCategory()
+
+renderCategory()
 
 
-function hidCategory(element){
-    dialog_catagory.style.display ="none";
-    
-}
-function showCategory(element){
-    dialog_catagory.style.display ="block";
-    
-}
-function storeCategory(){
-    let lisCategory = []
-}
-function Oncancell(){}
-function Oncreate(){}
 
-function createcategory(){
-    let tr = document.createElement("tr");
-    let tdcategoryID = document.createElement("td");
-    let tdcategoryName = document.createElement("td");
-    let tdAction = document.createElement("td");
-    let actionedite = document.createElement("div");
-    let actiondelate = document.createElement("div");
-    
-    tdcategoryID.textContent =idcategory.value;
-    tdcategoryName.textContent =CategoryName.value;
-    actionedite.src = "";
-    actiondelate.src = "";
-}
-// main
-let idcategory = document.getElementById("idcategory");
-let CategoryName = document.getElementById("Category Name");
-let Action = document.getElementById("Action");
---
+++ JS/order.js
@@ -0,0 +1,113 @@
+//=================== ProdacutData and CategoryData ===============
+
+let productData = JSON.parse(localStorage.getItem("product"))
+let categororyData = JSON.parse(localStorage.getItem("category"))
+
+//=================== Save and loard Order  =======================
+
+function saveOrder() {
+    localStorage.setItem("order", JSON.stringify(orderData.order))
+}
+function loadOrder() {
+    let OrderStorage = JSON.parse(localStorage.getItem("order"));
+    if (OrderStorage !==null){
+        orderData.order=OrderStorage
+    }
+}
+
+//========================== Search ID of Products ==================
+function searchIdProduct() {
+    let value_input = inputID.value;
+    for (let data of productData) {
+        if (value_input == data.id) {
+
+            let p_Name = document.createElement("p");
+            let p_Quantity = document.createElement("p");
+            let p_Price = document.createElement("p");
+            let span = document.createElement("span");
+
+            p_Name.textContent = data.name;
+            p_Quantity.textContent = "Quantity: ";
+            p_Price.textContent = data.price + "$";
+            span.textContent = data.quantity;
+
+            p_Quantity.appendChild(span);
+            headerBox.appendChild(p_Name);
+            bodyBox.appendChild(p_Quantity);
+            bodyBox.appendChild(p_Price);
+
+            let orderProducts = {
+                idOrder: data.id,
+                orderName: data.name,
+                orderCategory: data.category,
+                orderQuantity: data.quantity,
+                orderPrice: data.price,
+            }
+            orderData.order.push(orderProducts)
+            saveOrder()
+        }
+    }
+}
+//===================================== Display items order to table =================
+function addItemOrder() {
+    tbody.remove();
+    let newTbody = document.createElement("tbody");
+    let index = 0
+    for (let orderItem of orderData.order) {
+        let tRows = document.createElement("tr");
+        tRows.dataset.index = index;
+
+        let tdId = document.createElement("td");
+        let tdName = document.createElement("td")
+        let tdQuantity = document.createElement("td");
+        inputQnt = document.createElement("input");
+        inputQnt.type="number"
+        inputQnt.value=parseInt(orderItem.orderQuantity);
+        tdQuantity.appendChild(inputQnt);
+
+        let tdPrice = document.createElement("td");
+        let tdAction = document.createElement("td");
+        let iconDelete = document.createElement("i");
+        iconDelete.classList.add("material-icons");
+        iconDelete.textContent="delete"
+        tdAction.appendChild(iconDelete);
+
+        tdId.textContent=orderItem.idOrder;
+        tdName.textContent=orderItem.orderName;
+        tdPrice.textContent=orderItem.orderPrice;
+
+        tRows.appendChild(tdId);
+        tRows.appendChild(tdName);
+        tRows.appendChild(tdQuantity);
+        tRows.appendChild(tdPrice);
+        tRows.appendChild(tdAction);
+
+        newTbody.appendChild(tRows);
+    }
+    table.appendChild(newTbody);
+}
+
+let inputID = document.querySelector("#input-id");
+let btnSearch = document.querySelector("#search-id");
+btnSearch.addEventListener("click", searchIdProduct)
+
+let headerBox = document.querySelector(".header-box");
+let bodyBox = document.querySelector(".body-box");
+
+let table = document.querySelector("table")
+let tbody = document.querySelector("tbody");
+
+let btnAddtoCard = document.querySelector("#add-to-card");
+btnAddtoCard.addEventListener("click", addItemOrder)
+
+let total = document.querySelector("span")
+
+let btnCheckout = document.querySelector("#check-out");
+
+let orderData = {
+    order: [],
+}
+
+loadOrder()
+
+addItemOrder()
