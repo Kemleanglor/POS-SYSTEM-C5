f1cad735e53a8a8e6d4cf4d5c0a35e477f0a3452
BUNNARITHHHHH <bunnarith.phoeurn@student.passerellesnumeriques.org>
2023-12-20 14:48:34 +0700

    #23 add card to order

--- JS/order.js
+++ JS/order.js
@@ -15,6 +15,62 @@ function loadOrder() {
     }
 }
 
+
+//===================================== Display items order to table =================
+function addItemOrder() {
+    let tbody = document.querySelector("tbody");
+    tbody.remove();
+    let newTbody = document.createElement("tbody");
+    let index = 0;
+    // let price =0;
+
+    for (let orderItem of orderData.order) {
+        let tRows = document.createElement("tr");
+        tRows.dataset.index = index;
+
+        let tdId = document.createElement("td");
+        let tdName = document.createElement("td")
+        let tdQuantity = document.createElement("td");
+        inputQnt = document.createElement("input");
+        inputQnt.type="number"
+        inputQnt.value=orderItem.orderQuantity
+        tdQuantity.appendChild(inputQnt);
+
+        let tdPrice = document.createElement("td");
+        let tdAction = document.createElement("td");
+        let iconDelete = document.createElement("i");
+        iconDelete.classList.add("material-icons");
+        iconDelete.textContent="delete";
+        iconDelete.addEventListener("click",removeOrder);
+
+        tdAction.appendChild(iconDelete);
+
+        tdId.textContent=orderItem.idOrder;
+        tdName.textContent=orderItem.orderName;
+        tdPrice.textContent=orderItem.orderPrice+"$";
+
+        tRows.appendChild(tdId);
+        tRows.appendChild(tdName);
+        tRows.appendChild(tdQuantity);
+        tRows.appendChild(tdPrice);
+        tRows.appendChild(tdAction);
+
+        newTbody.appendChild(tRows);
+        index +=1
+        toTalPrice+=parseInt(orderItem.orderQuantity)*parseInt(orderItem.orderPrice);
+
+    }
+    table.appendChild(newTbody);
+    
+    total.textContent=toTalPrice+"$";
+
+    inputID.value=""
+    p_Name.textContent ="";
+    p_Quantity.textContent ="";
+    p_Price.textContent ="";
+    span.textContent ="";
+}
+
 //========================== Search ID of Products ==================
 function searchIdProduct() {
     let value_input = inputID.value;
